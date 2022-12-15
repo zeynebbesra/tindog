9aa79fcffd9d598cdd947fcc8001ea99999bebaf
zeynebbesra <394777@ogr.ktu.edu.tr>
2022-12-15 17:45:50 +0300

    token created for auth

--- dog/urls.py
+++ dog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main ),
-    path('dogs', views.get_dogs),
-    path('dogs/<str:id>/', views.get_dog)
+    #path('', views.main ),
+    path('dogs', views.get_dogs),   #dogs listesine dog ekliyor
+    path('dogs/add_dog', views.add_dog), #dogs listesini veriyor
+    path('dogs/<str:id>/', views.get_dog) #dog'u id'ye göre veriyor
 ]
 
